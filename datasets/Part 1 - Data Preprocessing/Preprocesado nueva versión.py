acc4d40a4632f74141274e0ea8a2744de69cea50
Andrés Felipe Palomino Montezuma <palomino.andres@correounivalle.edu.co>
2023-02-26 22:04:52 -0500

    Actualización

--- datasets/Part 1 - Data Preprocessing/Preprocesado nueva versión.py
+++ datasets/Part 1 - Data Preprocessing/Preprocesado nueva versión.py
@@ -1,12 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Apr 26 12:11:34 2020
-
-@author: juangabriel
-"""
-
-
 # Plantilla de Pre Procesado
 
 # Cómo importar las librerias en Python
