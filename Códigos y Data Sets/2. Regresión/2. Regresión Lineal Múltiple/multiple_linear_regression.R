04a0a5628a3430b4d2867c020551a3c2e05d440d
Andrés Felipe Palomino Montezuma <palomino.andres@correounivalle.edu.co>
2023-03-01 16:23:06 -0500

    Ajustes

--- Códigos y Data Sets/2. Regresión/2. Regresión Lineal Múltiple/multiple_linear_regression.R
+++ Códigos y Data Sets/2. Regresión/2. Regresión Lineal Múltiple/multiple_linear_regression.R
@@ -8,6 +8,23 @@ dataset = read.csv('50_Startups.csv')
 dataset$State = factor(dataset$State,
                          levels = c("New York", "California", "Florida"),
                          labels = c(1, 2, 3))
+backwardElimination <- function(x, sl) {
+  numVars = length(x)
+  for (i in c(1:numVars)){
+    regressor = lm(formula = Profit ~ ., data = x)
+    maxVar = max(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"])
+    if (maxVar > sl){
+      j = which(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"] == maxVar)
+      x = x[, -j]
+    }
+    numVars = numVars - 1
+  }
+  return(summary(regressor))
+}
+
+SL = 0.05
+dataset = dataset[, c(1,2,3,4,5)]
+backwardElimination(training_set, SL)
 
 
 
@@ -49,4 +66,22 @@ summary(regression)
 
 regression = lm(formula = Profit ~ R.D.Spend,
                 data = dataset)
+###########3
 summary(regression)
+backwardElimination <- function(x, sl) {
+  numVars = length(x)
+  for (i in c(1:numVars)){
+    regressor = lm(formula = Profit ~ ., data = x)
+    maxVar = max(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"])
+    if (maxVar > sl){
+      j = which(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"] == maxVar)
+      x = x[, -j]
+    }
+    numVars = numVars - 1
+  }
+  return(summary(regressor))
+}
+
+SL = 0.05
+dataset = dataset[, c(1,2,3,4,5)]
+backwardElimination(training_set, SL)
